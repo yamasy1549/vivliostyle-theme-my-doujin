f8d55ca60b8dc1710818acba5bfd3428a44e04b3
yamasy1549 <sanae@yamasy.info>
2021-04-09 19:37:23 +0900

    ⑤目次

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -2,7 +2,14 @@ module.exports = {
   language: 'ja',
   theme: 'theme_print.css',
   entry: [
+    {
+      // 目次
+      rel: 'contents',
+      theme: 'theme_toc.css',
+    },
     'example/ch01.md',
     'example/ch02.md',
   ],
+  toc: true,
+  tocTitle: '目次',
 }
