66a59964e35d2c1bf303cbf8319e5fd8bc57ab35
yamasy1549 <sanae@yamasy.info>
2021-04-09 19:39:37 +0900

    ⑥原稿ごとのテーマカラー

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -7,8 +7,14 @@ module.exports = {
       rel: 'contents',
       theme: 'theme_toc.css',
     },
-    'example/ch01.md',
-    'example/ch02.md',
+    {
+      path: 'example/ch01.md',
+      theme: 'theme_ch01.css',
+    },
+    {
+      path: 'example/ch02.md',
+      theme: 'theme_ch02.css',
+    },
   ],
   toc: true,
   tocTitle: '目次',
